9e6d5295e9e21493745041c0212e39fea92aaa3b
kavishka sandeepa <kavishkapk99@gmail.com>
2024-03-01 11:50:05 +0530

    weather fetch Api almost done

--- src/Component/Main.jsx
+++ src/Component/Main.jsx
@@ -2,7 +2,7 @@ import './Main.css'
 import weather from '../assets/01.png'
 export default function Min() {
 
-    const apiKey=fc377b6c97c3bdc3e927040de7d9eeb9;
+    const apiKey="fc377b6c97c3bdc3e927040de7d9eeb9";
     const cityName=null;
 
     async function  searchCity(){
@@ -13,10 +13,18 @@ export default function Min() {
         }
 
        let response= await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputEle[0].value}&units=metric&appid=${apiKey}`);
-       let data =await response.jason();
-        
+       let data =await response.json();
 
+       const temp=document.getElementsByClassName("tepm");
+       const city=document.getElementsByClassName("city");
+       const humidiy=document.getElementsByClassName("humidity-precent");
+       const wind=document.getElementsByClassName("wind-speed");
         
+        temp[0].innerHTML=data.main.temp;
+        city[0].innerHTML=data.name;
+        humidiy[0].innerHTML=data.main.humidity;
+        wind[0].innerHTML=data.wind.speed;
+
 
     }
 
@@ -24,7 +32,7 @@ export default function Min() {
         <div className="container mt-2 dec">
             <div className='row'>
                 <div className='col-6'>
-                    <input  type="text" className="form-control mt-5 cityInput" id="exampleFormControlInput1" placeholder="Country"></input>
+                    <input  type="text" className="form-control mt-5 cityInput" id="exampleFormControlInput1" placeholder="City Name"></input>
                 </div>
                 <div className=' col-6'>
                     <button onClick={()=>{searchCity()}} type="button" className="btn btn-warning btnCustom btn-x ">Search</button>
@@ -36,21 +44,21 @@ export default function Min() {
             <div className='center'>
 
                 <img src={weather} />
-                <h1>24°C</h1>
-                <h3>London</h3>
+                <h1 className='tepm'>24°C</h1>
+                <h3 className='city'>London</h3>
 
             </div>
 
             <div className='bottom row'>
                 <div className='humidity col-6'>
                     <i class="bi bi-body-text humidity-icon"></i>
-                    <h4>74%</h4>
+                    <h4 className='humidity-precent'>74%</h4>
                     <h5>Humidity</h5>
 
                 </div>
                 <div className='wind col-6'>
                     <i className="bi bi-wind wind-icon"></i>
-                    <h4>24 kmph</h4>
+                    <h4 className='wind-speed'>24 kmph</h4>
                     <h5>Wind Speed</h5>
 
                 </div>
